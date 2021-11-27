4d4771e5e33d5ecd6022ddd695e76dd78b1eb86e
zairulmazwan <zairulmazwan@localhost>
2021-11-27 18:52:51 +0000

    v1.0

---
+++ Tutorial10_TSP_GeneticAlgorithm/src/Data.java
@@ -0,0 +1,154 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+
+public class Data {
+	
+	
+	public static double [][] genData(int n){
+		
+		double [][] res = new double [n][n];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				
+				if(i==j) {
+					res[i][j] = 0;
+				}
+				else {
+					Random r = new Random();
+					double num = (double)(r.nextInt(10000)+ 10)/100;
+					//DecimalFormat df = new DecimalFormat("#0.00");
+					res[i][j] = num;
+					res[j][i] = num;
+				}
+			}
+		}
+		return res;
+	}
+	
+	public static void printArray (double [][] data) {
+		
+		for(int i=0; i<data.length; i++) {
+			for(int j=0; j<data[i].length; j++) {
+				System.out.print(data[i][j]+"\t");
+				
+			}
+			System.out.println();
+		}
+	}
+	
+	public static double [][] readFile (String fileName){
+		
+		double [][] res = null;
+		
+		try {
+			
+			FileReader fr = new FileReader (fileName);
+			BufferedReader br = new BufferedReader (fr);
+			
+			int col=0, row=0;
+			String line = null;
+			
+			//need to know how many rows and cols in the csv file
+			while ((line = br.readLine()) != null)
+			{
+				row++;
+				String [] column = line.split(",");
+				col = column.length;
+				
+			}
+				
+			int i=0, j=0;
+			res = new double [row][col]; //set the size of the array after we have known the row and col
+			//System.out.println(col+" "+row);
+			
+			br = new BufferedReader (new FileReader (fileName)); //this has to be initiated again!
+			
+			while ((line = br.readLine()) != null)
+			{
+				String [] column = line.split(",");
+				
+				for (j=0; j<column.length; j++)
+				{
+					res [i][j] = Double.parseDouble(column [j]) ;
+				}
+				
+				i++;		
+			}
+			
+		}
+		catch(Exception e) {
+			System.err.print("Error");
+		}
+		
+		
+		return res;
+	}
+	
+	public static void writeData(double [][] d) {
+		
+		String fn = "/Volumes/UBUNTU 20_0/Backup - HP/TSP_Data/data10.csv";
+		
+		try {
+			
+			FileWriter fw = new FileWriter(fn);
+			BufferedWriter bw = new BufferedWriter(fw);
+	
+			for (int i=0; i<d.length; i++) {
+				for(int j=0; j<d[i].length; j++) {
+					
+					bw.write(Double.toString(d[i][j]));
+					
+					if(j!=d[i].length-1) bw.write(","); //however in java, if there is no value after a comma, it will not be counted as length. The if condition might not necessary
+				}
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+		}
+		catch(Exception e) {
+			System.err.print("Error");
+		}
+	}
+	
+	public static void writeResult(String file, GeneticAlgo.Population result) {
+		
+		try {
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter (fw);
+			
+			for (int i=0; i<result.population.size(); i++) {
+				
+				for (int j=0; j<result.population.get(i).chromosome.size(); j++) {
+					bw.write(Integer.toString(result.population.get(i).chromosome.get(j)));
+					bw.write(" ");
+				}
+				bw.write(Double.toString(result.population.get(i).fitness));
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+			
+		}
+		catch(Exception e) {
+			System.err.print("Error");
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		double [][] data = genData(10);
+		//printArray(data);
+		writeData(data);
+		double [][] d = readFile("/Volumes/UBUNTU 20_0/Backup - HP/TSP_Data/data10.csv");
+		printArray(d);
+	}
+
+}
---
+++ Tutorial10_TSP_GeneticAlgorithm/src/GeneticAlgo.java
@@ -0,0 +1,242 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+
+public class GeneticAlgo {
+	
+	static double [][] dataset;
+	
+	
+	class Individual{
+		
+		public ArrayList<Integer> chromosome = new ArrayList<Integer>();
+		double fitness;
+		
+		public Individual(int n) {
+			
+			Set<Integer> chromosome = new LinkedHashSet<Integer>();//we use set to get unique number within the range
+			
+			Random r = new Random();
+			
+			while(chromosome.size()<n) {
+				chromosome.add(r.nextInt(n));
+			}
+			
+			this.chromosome = new ArrayList<Integer>(chromosome); //convert set into arrayList
+			setFitness();
+		}
+		
+		public void setFitness () {
+			
+			double res = 0;
+			int size = chromosome.size();
+			
+			for(int i=0; i<size-1; i++) {
+				int from = chromosome.get(i);
+				int to = chromosome.get(i+1);
+				
+				//System.out.println(from+" "+to);
+				//System.out.println(dataset[from][to]);
+				
+				res += dataset[from][to];
+			}
+			//System.out.println(ind.chromosome.get(size-1)+" "+ind.chromosome.get(0));
+			res+= dataset[chromosome.get(size-1)][chromosome.get(0)]; //x[n][0] - return to the origin
+			//System.out.println(res);
+				
+			this.fitness = res;
+			
+		}
+		
+		public Individual copyGenes(Individual ind) {
+			
+			Individual res = new Individual(ind.chromosome.size());
+			
+			for (int i=0; i<ind.chromosome.size(); i++) {
+				res.chromosome.set(i, ind.chromosome.get(i));
+			}
+			
+			return res;
+		}
+		
+		public void printChromosome () {
+			
+			System.out.print(chromosome+" "+fitness);
+		
+		}
+		
+	}
+
+	class Population{
+		
+		public ArrayList<Individual> population = new ArrayList<Individual>();
+		
+		public Population (int popSize, int chromosomeSize) {
+		
+			for (int i=0; i<popSize; i++) {
+				Individual ind = new Individual(chromosomeSize);
+				this.population.add(ind);
+			}
+		}
+		
+		public void printPop() {
+			
+			for (int i=0; i<population.size(); i++) {
+				System.out.print(population.get(i).chromosome+"\t");
+				System.out.println(population.get(i).fitness);
+			}
+		}
+		
+		public Individual crossOver (Individual p1, double rate) {
+			
+			//Declate a chromosome variable (use Set<Integer>,  new LinkedHashSet<Integer>)
+			Set<Integer> chromosome = new LinkedHashSet<Integer>();//we use set to get unique number within the range
+			
+			//Declare the result variable (should be Individual type), the res size should be the parent's size
+			Individual res = new Individual(p1.chromosome.size());
+			
+			//Get the crossover point. Use the rate. Value of crossover point should be int
+			int point = (int)(p1.chromosome.size()*rate);
+		
+			//A for loop to copy the genetic from the parent up to the crossover point
+			for (int i=0; i<point; i++) {
+				chromosome.add(p1.chromosome.get(i)); //copy some genes from the parent
+			}
+			
+			//Now to get the remaining genes by random. Use while loop to get the remaining genes
+			Random r = new Random();
+			while(chromosome.size()<p1.chromosome.size()) {
+				chromosome.add(r.nextInt(p1.chromosome.size())); //get the remaining genes by random
+			}
+			
+			//Since our chromosome in Individual class is ArrayList, convert the Set variable into ArrayList
+			res.chromosome = new ArrayList<Integer>(chromosome); //convert set into arrayList
+			
+			return res;
+		}
+		
+		public Individual mutate (Individual p1) {
+			
+			//Copy the genetic from p1 to our initial result
+			Individual res 
+			
+			//now we mutate the genes, using small change (swap genes by random)
+			
+			
+			
+			//Use a while loop to regenerate random j to avoid getting the same gene. WARNING : This is not application is the remaining genes is only 1
+			
+
+			
+			
+			
+			
+			//Swap the value between i and j for our mutate chromosome
+			
+			
+			
+			return res;
+		}
+
+		
+	}
+	
+	public static void runGA() {
+		
+		//create a population object and parameters
+		
+		
+		
+		//prepare dataset
+		
+		
+		
+		//initialise the population
+		
+		
+		//We sort the candidates by fitness in ascending order, the least the better in this example (TSP)
+		
+		
+		
+		//Find solution in the generations
+		for (int gen=0; gen<numGeneration; gen++) {
+			
+			System.out.println("Generation : "+gen);
+			
+			//get the parents - top 2 from the list
+			
+			
+			
+			//get 2 new children
+			
+			
+			
+			
+			//get a mutate child
+			
+			
+			
+			
+			//add these new children to the population
+			
+			
+			
+			
+			
+			//sort them
+			
+
+			
+			
+			
+			//remove the weak candidate
+			
+			
+			
+			
+			
+		}
+		
+		System.out.println("====Result====");
+	
+		
+		//Write the result into a csv file
+		
+		
+	
+	}
+	
+	public static double calFitness (ArrayList<Integer> x) {
+		double res = 0;
+		int size = x.size();
+		
+		for(int i=0; i<size-1; i++) {
+			int from = x.get(i);
+			int to = x.get(i+1);
+			
+			//System.out.println(from+" "+to);
+			//System.out.println(dataset[from][to]);
+			
+			res += dataset[from][to];
+		}
+		//System.out.println(ind.chromosome.get(size-1)+" "+ind.chromosome.get(0));
+		res+= dataset[x.get(size-1)][x.get(0)]; //x[n][0] - return to the origin
+		//System.out.println(res);
+			
+		return res;
+	}
+
+	public static void main(String[] args) {
+		
+		runGA();
+		
+		double [][] mst = MST.PrimsMST(dataset);
+		System.out.println("\nMST fitness : "+MST.getMstValue(mst));
+		
+
+	}
+
+}
